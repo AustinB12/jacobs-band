78ce2794beb72255bd882a72e8f15f7803414c01
Austin B <austinbaird.unc@gmail.com>
2020-04-03 12:14:32 -0400

    Header + Footer

--- jacobs-band/src/App.js
+++ jacobs-band/src/App.js
@@ -1,40 +1,16 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from "react";
 
-import HomePage from './pages/homePage/home';
-import AboutPage from './pages/about/about';
-import Footer from './components/footer/footer';
+import Footer from "./components/footer/footer";
+import Header from "./components//header/header";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="App-header">
-          
-          <p>
-            Website Under Construction for Jacobs Band!
-          </p>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </div>
-          <Switch className='page-switch'>
-            <Route path="/about">
-              <AboutPage />
-            </Route>
-            <Route path="/">
-            <HomePage />
-            </Route>
-          </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
